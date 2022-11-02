04e98ed09b519d5f676376b56056410308d963f0
LeoneBacciu <leonebacciu@gmail.com>
2022-11-02 17:22:05 +0100

    Add windows getline polyfill

--- utils.h
+++ utils.h
@@ -5,6 +5,14 @@
 #ifndef SNAKE_LABYRINTH_UTILS_H
 #define SNAKE_LABYRINTH_UTILS_H
 
+#include <malloc.h>
+#include <stdio.h>
+
 int clamp(int low, int value, int high);
+
+
+#ifdef _WIN32
+__size_t getline(char **lineptr, size_t *n, FILE *stream);
+#endif
 
 #endif //SNAKE_LABYRINTH_UTILS_H
