01c523248e94cf3e9c73e62a71cdbbf9fc993a81
Leone Bacciu <leonebacciu@gmail.com>
2022-11-06 14:53:01 +0100

    Fix windows compatibility

--- utils.h
+++ utils.h
@@ -1,18 +1,6 @@
-//
-// Created by stark on 27/10/22.
-//
-
-#ifndef SNAKE_LABYRINTH_UTILS_H
-#define SNAKE_LABYRINTH_UTILS_H
+#pragma once
 
 #include <malloc.h>
 #include <stdio.h>
 
 int clamp(int low, int value, int high);
-
-
-#ifdef _WIN32
-size_t getline(char **lineptr, size_t *n, FILE *stream);
-#endif
-
-#endif //SNAKE_LABYRINTH_UTILS_H
