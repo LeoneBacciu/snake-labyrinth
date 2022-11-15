a802a10da46d730311054d5e6461cbf13e4fd522
LeoneBacciu <leonebacciu@gmail.com>
2022-11-15 09:26:42 +0100

    Optimize algorithm

--- ai/solver.c
+++ ai/solver.c
@@ -9,12 +9,13 @@ path_values_t solve(maze_state_t *maze) {
         for (int c = 0; c < maze->matrix->cols; ++c)
             if (maze_get(maze, c(c, r))) max_bonus++;
 
-    matrix_t *cache = matrix_create(maze->matrix->cols, maze->matrix->rows, INT_MIN);
+    matrix_t *highest = matrix_create(maze->matrix->cols, maze->matrix->rows, INT_MIN);
+    matrix_t *shortest = matrix_create(maze->matrix->cols, maze->matrix->rows, INT_MAX);
 
     heap_t *heap = heap_create(1000);
 
     heap_insert(heap, 0, maze_copy(maze));
-    matrix_set(cache, cx(maze->pos), 0);
+    matrix_set(highest, cx(maze->pos), 0);
 
     while (!heap_empty(heap)) {
         maze_state_t *state = heap_extract(heap);
@@ -25,7 +26,7 @@ path_values_t solve(maze_state_t *maze) {
                 best = path_values(state->path);
                 best_score = score;
             }
-            if (state->coins == max_bonus || !can_outperform(best_score, max_bonus, state)) break;
+            if (state->coins == max_bonus && !can_outperform(best_score, max_bonus, state)) break;
 
             maze_free(state);
 
@@ -35,11 +36,14 @@ path_values_t solve(maze_state_t *maze) {
         for (int i = 0; i < 4; ++i) {
             coord_t n_pos = c_add(state->pos, movements[i]);
 
-            if (!maze_can_go(state, n_pos) || matrix_get(cache, cx(n_pos)) >= score) continue;
+            if (!maze_can_go(state, n_pos) ||
+                matrix_get(shortest, cx(n_pos)) <= state->steps && matrix_get(highest, cx(n_pos)) >= score)
+                continue;
 
             maze_state_t *n_state = maze_copy_move(state, i);
 
-            matrix_set(cache, cx(n_pos), score);
+            matrix_set_max(highest, cx(n_pos), score);
+            matrix_set_min(shortest, cx(n_pos), score);
 
             heap_insert(heap, maze_score(n_state), n_state);
         }
