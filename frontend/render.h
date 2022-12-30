29222f2b5a64a244d0e65394fa5f60dbd45b5642
LeoneBacciu <leonebacciu@gmail.com>
2022-12-30 18:31:48 +0100

    Add optional menu

--- frontend/render.h
+++ frontend/render.h
@@ -5,7 +5,10 @@
 #include <stdlib.h>
 #include "maze/maze.h"
 #include <ncurses.h>
+
+#ifdef USE_MENU
 #include <menu.h>
+#endif
 
 #define IS_VERTEX(c, r, ratio) (((r % ratio == 0) + (c % ratio == 0) + (r % ratio == ratio - 1) + (c % ratio == ratio - 1)) == 2)
 
@@ -25,6 +28,8 @@ void render_maze(maze_state_t *maze);
 
 void render_end_game(maze_state_t *maze);
 
+#ifdef USE_MENU
 int render_menu(char *title, int choices_count, char **choices);
+#endif
 
 void render_replay(maze_state_t *maze);
